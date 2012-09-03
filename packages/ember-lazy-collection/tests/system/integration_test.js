9bdb1cd53d170c81923d82ad3623beeac3e09a23
Stefan Penner <stefan@Samantha-Rechs-MacBook-Pro.local>
2012-09-03 11:58:14 -0400

    initial commit

---
+++ packages/ember-lazy-collection/tests/system/integration_test.js
@@ -0,0 +1,8 @@
+module("ember-lazy-collection.integration",{
+  setup: function(){ },
+  tearDown: function(){ }
+});
+
+test("everything is ok", function(){
+  ok(true, "all ok");
+});
---
+++ packages/ember-lazy-collection/tests/system/unit_test.js
@@ -0,0 +1,8 @@
+module("ember-lazy-collection.unit",{
+  setup: function(){ },
+  tearDown: function(){ }
+});
+
+test("everything is ok", function(){
+  ok(true, "all ok");
+});
